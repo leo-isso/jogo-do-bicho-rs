b719d8b1212327a87ecbc8ad3ba7df55c9015ab7
Leonardo Isso <leoisso.work@gmail.com>
2023-08-16 01:36:49 -0300

    feta: init commit and strucutres

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+struct Round {
+    id: String,
+    draws: Vec<u16>,
+} // generate_draws, get_prize, get_winners
+
+struct Gambler {}
+struct Bet {}
+struct BetType {} // Default, Quina, Milhar + validacoes
+
+fn main() {
+    println!("Hello, world!");
+}
