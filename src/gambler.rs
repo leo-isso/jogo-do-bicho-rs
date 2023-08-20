a406d51951d9a6ced854b48935c7a913aa1f2e4c
Leonardo Isso <leoisso.work@gmail.com>
2023-08-20 19:32:16 -0300

    feat: add place_bet to Gambler Impl

--- src/gambler.rs
+++ src/gambler.rs
@@ -1,5 +1,7 @@
 use rand::Rng;
 
+use crate::{bet::{Bet, BetType}, round::Round};
+
 pub struct Gambler {
     pub id: String,
     pub name: String,
@@ -12,5 +14,14 @@ impl Gambler {
             id: rng.gen::<u32>().to_string(),
             name,
         }
+    }
+
+    pub fn place_bet<'round_lt, 'gambler_lt>(
+        &'gambler_lt self,
+        value: u32,
+        round: &'round_lt Round,
+        bet_type: BetType
+    ) -> Bet<'round_lt, 'gambler_lt> {
+        Bet::new(value, &round, &self, bet_type)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod bet;
 pub mod gambler;
 pub mod round;
 
-use crate::bet::{Bet, BetType};
+use crate::bet::{BetType};
 use crate::gambler::Gambler;
 use crate::round::Round;
 
@@ -13,7 +13,7 @@ fn main() {
 
     let gambler_2 = Gambler::new(String::from("Jack"));
 
-    let bet_1 = Bet::new(1000, &round, &gambler_1, BetType::Bicho);
+    let bet_1 = gambler_1.place_bet(1000, &round, BetType::Bicho);
 
-    let bet_2 = Bet::new(1000, &round, &gambler_2, BetType::Bicho);
+    let bet_2 = gambler_2.place_bet(1000, &round, BetType::Bicho);
 }
