581d492335216163c34d44f3505d1aa65eaaecf8
Leonardo Isso <leoisso.work@gmail.com>
2023-08-20 19:47:22 -0300

    fix: adjust to rustacean patterns

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod bet;
 pub mod gambler;
 pub mod round;
 
-use crate::bet::{BetType};
+use crate::bet::BetType;
 use crate::gambler::Gambler;
 use crate::round::Round;
 
@@ -13,7 +13,7 @@ fn main() {
 
     let gambler_2 = Gambler::new(String::from("Jack"));
 
-    let bet_1 = gambler_1.place_bet(1000, &round, BetType::Bicho);
+    let _bet_1 = gambler_1.place_bet(1000, &round, BetType::Bicho);
 
-    let bet_2 = gambler_2.place_bet(1000, &round, BetType::Bicho);
+    let _bet_2 = gambler_2.place_bet(1000, &round, BetType::Bicho);
 }
